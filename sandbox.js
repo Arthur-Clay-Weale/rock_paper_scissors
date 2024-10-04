e468b095990c1c38977e6c29b3ffad9002a1d3bb
Arthur Weale <ajayrossy340@gmail.com>
2024-10-04 02:56:44 +0200

    Deleted the first structure i made because i was copying....but i am figuring stuff out

--- sandbox.js
+++ sandbox.js
@@ -2,9 +2,25 @@
 
 
 
+function getComputerChoice(){
+    let randomNum = Math.floor(Math.random() * 3);
 
+    if(randomNum === 0){
+        return 'Rock';
+    }else if (randomNum === 1){
+        return 'Paper';
+    }else return 'Scissors';
+    
+    
+}
 
+for(let i = 0; i < 3; i++){
+    console.log(getComputerChoice())
+}
 
+function getHumanChoice(){
+    choice = prompt('What do you choose: ');
+    return choice;
+}
 
-
-//OK its about to get Fun.
+//console.log(getHumanChoice())
