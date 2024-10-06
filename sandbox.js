7f13fed9cf20fc4c8e7e7a8cd001fe7bb6263bef
Arthur Weale <ajayrossy340@gmail.com>
2024-10-06 02:21:26 +0200

    Forgot to commit my changes. Added the loop to make the game initiate 5 times. Added score board

--- sandbox.js
+++ sandbox.js
@@ -1,26 +1,67 @@
 
 
-
+let humanScore = 0;
+let computerScore = 0;
+for(let i = 0; i < 5; i++){
 
 function getComputerChoice(){
     let randomNum = Math.floor(Math.random() * 3);
 
     if(randomNum === 0){
-        return 'Rock';
+        return 'rock';
     }else if (randomNum === 1){
-        return 'Paper';
-    }else return 'Scissors';
+        return 'paper';
+    }else return 'scissors';
     
     
-}
-
-for(let i = 0; i < 3; i++){
-    console.log(getComputerChoice())
-}
-
+} 
 function getHumanChoice(){
     choice = prompt('What do you choose: ');
-    return choice;
+    return choice.toLowerCase();
 }
+function playRound(humanChoice, computerChoice){
 
-//console.log(getHumanChoice())
+    
+    if(computerChoice == humanChoice) {
+        return 'Its a Tie!';
+    } else if(computerChoice == 'rock' && humanChoice == 'paper') {
+        return 'Player wins, paper beats rock';
+    } else if(computerChoice == 'paper' && humanChoice == 'scissors') {
+        return 'Player wins, scissors beats paper';
+    } else if(computerChoice == 'scissors' && humanChoice == 'rock') {
+        return 'Player wins, rock beats scissors';
+    } else if(computerChoice == 'rock' && humanChoice == 'scissors') {
+        return 'Computer wins, rock beats scissors';
+    } else if(computerChoice == 'paper' && humanChoice == 'rock') {
+        return 'Computer wins, paper beats rock';
+    } else if(computerChoice == 'scissors' && humanChoice == 'paper') {
+        return 'Computer wins, scissors beats paper';
+    }
+}
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+console.log(playRound(humanSelection.toLowerCase(),computerSelection.toLowerCase()));
+
+function updateScoreBoard(){
+
+    if(humanSelection =='paper' && computerSelection == 'rock'){
+        humanScore += 1;
+    }else if(humanSelection =='scissors' && computerSelection == 'paper'){
+        humanScore += 1;
+    }else if(humanSelection =='rock' && computerSelection == 'scissors'){
+        humanScore += 1;
+    }
+
+    if(humanSelection =='rock' && computerSelection == 'paper'){
+        computerScore += 1;
+    }else if(humanSelection =='paper' && computerSelection == 'scissors'){
+        computerScore += 1;
+    }else if(humanSelection =='scissors' && computerSelection == 'rock'){
+        computerScore += 1;
+    }
+
+    console.log('Player score is: '+ humanScore);
+    console.log('Computer score is: '+computerScore);
+}
+updateScoreBoard();
+}
